2519d0788e6ad9a30cfb1b6f10937fc661fa24a0
laurenshareshian <laurenshareshian@gmail.com>
2019-11-17 18:25:18 -0800

    all images rendering!

--- src/server.js
+++ src/server.js
@@ -13,6 +13,16 @@ app.use(express.static('public'));
 app.get('/api/getcandidates', (req, res) => {
     var candidates = [
         {id: 1, name: 'Joe Biden', title: "Former Vice President", website: "https://joebiden.com/", pic: 'biden.png'},
+        {id: 2, name: 'Cory Booker', title: 'U.S. Senator, New Jersey', website: "https://corybooker.com/", pic: 'booker.jpg'},
+        {id: 3, name: 'Pete Buttigieg', title: 'Mayor, South Bend, IN', website: 'https://peteforamerica.com/', pic: 'buttigieg.png'},
+        {id: 4, name: "Julian Castro", title: "Former Secretary of Housing and Urban Development", website: "https://www.julianforthefuture.com/", pic: 'castro.jpg'},
+        {id: 5, name: "Tulsi Gabbard", title: "U.S. Representative, Hawaii", website: 'https://www.tulsi2020.com', pic: 'gabbard.jpg'},
+        {id: 6, name: "Kamala Harris", title: "U.S. Senator, California", website: 'https://kamalaharris.org/', pic: 'harris.jpg'},
+        {id: 7, name: 'Amy Klobuchar', title: "U.S. Senator, Minnesota", website: 'https://amyklobuchar.com/', pic: 'klobuchar.jpg'},
+        {id: 8, name: 'Bernie Sanders', title: "U.S. Senator, Vermont", website: 'https://berniesanders.com/', pic: 'sanders.png'},
+        {id: 9, name: 'Tom Steyer', title: "Hedge fund manager", website: "https://tomsteyer.com/", pic: 'steyer.png'},
+        {id: 10, name: "Elizabeth Warren", title: "U.S. Senator, Massachusetts", website: 'https://elizabethwarren.com/', pic: 'warren.jpg'},
+        {id: 11, name: 'Andrew Yang', title: 'Entrepeneur', website: "http://andrewyang.com/", pic: 'yang.jpg'},       
     ];
     // const candidatesStr = JSON.parse(candidates);
     console.log(candidates);
