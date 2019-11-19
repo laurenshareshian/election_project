fb1104242a9c3c93894348fde62480012d511b2e
laurenshareshian <laurenshareshian@gmail.com>
2019-11-18 17:46:49 -0800

    Start heroku setup

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
+const port = process.env.PORT || 8080;
 
 module.exports = {
   dev: {
@@ -19,7 +20,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
